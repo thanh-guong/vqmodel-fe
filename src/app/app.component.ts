e065d6b0dd8888cd39b7b49e5db4c94e0e3d74af
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-03 15:58:04 +0200

    Removed navbar component and moved all to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vqmodel';
+  aircraftRoute = environment.aircraftRoute;
+
+  public aircrafts =
+    {
+      name: 'Aircrafts',
+      icon: 'airplanemode_active'
+    };
+
+  public factory =
+    {
+      name: 'Factory',
+      icon: 'construction'
+    };
+
+  public reviews =
+    {
+      name: 'Reviews',
+      icon: 'book_online'
+    };
+
+  public distributors =
+    {
+      name: 'Distributors',
+      icon: 'send'
+    };
+
+  public contactUs =
+    {
+      name: 'Contact us',
+      icon: 'email'
+    };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AircraftsComponent } from './aircrafts/aircrafts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     AircraftsComponent
   ],
   imports: [
